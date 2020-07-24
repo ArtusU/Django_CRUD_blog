c3b5447dca546413cc5e62f43cdc82f147cca56c
ArtusU <artusrock@hotmail.com>
2020-07-24 17:19:52 +0100

    delete

--- src/posts/views.py
+++ src/posts/views.py
@@ -26,6 +26,25 @@ def post_create(request):
         form.save()
         return HttpResponseRedirect('/posts')
     context = {
-        "form": form
+        "form": form,
+        'form_type': 'Create'
     }
     return render(request, "post_create.html", context)
+
+
+def post_update(request, post_id):
+    post = Post.objects.get(id=post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/posts')
+    context = {
+        "form": form,
+        'form_type': 'Update'
+    }
+    return render(request, "post_create.html", context)
+
+def post_delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/posts')
